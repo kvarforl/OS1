0461103183a63bdaa573398cd3ad5b10a41c5d99
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-01-31 12:25:20 -0800

    added directory

--- Program2/kvarforl.buildrooms.c
+++ Program2/kvarforl.buildrooms.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+//for dir creation
+#include <sys/stat.h>
+
+//for pid
+#include <unistd.h>
+#include <sys/types.h>
+
+
 int main()
 {
-    printf("Hello world!\n");
     
+    char dirname[30];
+
+    int pid = getpid();
+    sprintf(dirname, "kvarforl.buildrooms.%i", pid);
     
+    if(mkdir(dirname, 0755) == -1)
+    {
+        printf("ERROR - could not make directory\n");
+    }
     
     return 0;
 }

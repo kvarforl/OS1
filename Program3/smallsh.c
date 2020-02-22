4d992c1a733ccf60a958808584711585038cdc10
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-02-21 16:12:55 -0800

    added fork/exec, status

--- Program3/smallsh.c
+++ Program3/smallsh.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //returns the number of tokens gotten from input
 //populates res (string arr) with each space delimited word of input
@@ -37,8 +39,8 @@ void builtinCd(char tokens[512][40], int num_tokens)
         if (chdir(home) != 0)
         { 
             printf("Chdir to home failed\n");
+            fflush(stdout);
         }
-        printf("changed to home: %s\n", home);
     }
     else 
     {
@@ -50,13 +52,29 @@ void builtinCd(char tokens[512][40], int num_tokens)
     }
 }
 
+void execute(char tokens[512][40], int num_tokens)
+{
+    char* args[num_tokens];
+    int i;
+    for(i=0;i<num_tokens;i++)
+    {
+        args[i] = (char*) &tokens[i];
+    }
+    args[num_tokens] = NULL;
+    if(execvp(args[0],args)< 0)
+    {
+        printf("Exec failed! Exiting. \n");
+        exit(1);
+    }
+}
 
 int main()
 {
     char tokens[512][40];
     char input_str[2048];
     int shell_running = 1;    
- 
+    int status = 0;
+
     while(shell_running)//for testing 
     {
         int num_tokens = getTokenizedInput(input_str, tokens);
@@ -75,7 +93,7 @@ int main()
         }
         else if(strcmp(tokens[0], "status")==0)
         {
-            printf("builtin status\n");
+            printf("exit status %i\n", status);
         }
         else if(strcmp(tokens[0], "exit")==0)
         {
@@ -86,7 +104,24 @@ int main()
         //fork and exec; let bash handle it. 
         else
         {    
-            printf("Other cmd: %s; check other tokens.\n", tokens[0]);
+            //printf("Other cmd: %s; check other tokens.\n", tokens[0]);
+            int childExitStatus;
+            pid_t spawnPID = fork();
+            switch(spawnPID)
+            {
+                case 0:
+                    //printf("In Child process: converting\n");
+                    //fflush(stdout);
+                    execute(tokens, num_tokens);
+                    break;
+                default:
+                    //printf("PARENT(%d): Wait()ing for child(%d) to terminate\n", getpid(), spawnPID);
+                    //pid_t actualPid = waitpid(spawnPID, &childExitStatus, 0);
+                    waitpid(spawnPID, &childExitStatus, 0);
+                    status = WEXITSTATUS(childExitStatus);
+                    //printf("PARENT(%d): Child(%d) terminated, Exiting!\n", getpid(), actualPid);
+                    break;
+            }
         }
     }
 
